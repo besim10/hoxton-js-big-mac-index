f8e9aef1d2b3f25165119487d3388a5f993401ba
Besim <besimsokoli10@gmail.com>
2021-11-15 19:03:39 +0100

    some basic changes

--- index.js
+++ index.js
@@ -31,3 +31,29 @@ if(norwayIndex > baseCurrency){
 }else{
     console.log("The Norway kruna is " + norwayIndex + " overvalued against the US dollar")
 }
+
+let danishActualExchangeRate = 6.53
+let danishPpp = denmarkBigMacPrice / uSBigMacPrice
+
+pppFixed = danishPpp.toFixed(2)
+
+let danishIndex = ((danishActualExchangeRate - pppFixed) / danishActualExchangeRate) * 100
+
+if(danishIndex > baseCurrency){
+    console.log("The Denmark kruna is " + danishIndex + " undervalued against the US dollar")
+}else{
+    console.log("The Denmark kruna is " + danishIndex + " overvalued against the US dollar")
+}
+
+let ucraineActualExchangeRate = 26.36
+let ucrainePpp = ucraineBigMacPrice / uSBigMacPrice
+
+pppFixed = ucrainePpp.toFixed(2)
+
+let ucarineIndex = ((ucraineActualExchangeRate - pppFixed) / ucraineActualExchangeRate) * 100
+
+if(ucarineIndex > baseCurrency){
+    console.log("The Ucraine hryvnia is " + ucarineIndex + " undervalued against the US dollar")
+}else{
+    console.log("The Ucraine hryvnia is " + ucarineIndex + " overvalued against the US dollar")
+}
